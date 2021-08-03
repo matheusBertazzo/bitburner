09b76bd700dd5dad3eb1b98e07e834869e7d4896
unknown <matheus.j.bertazzo@gmail.com>
2021-08-03 03:32:30 -0300

    added enrich-discovery.script

---
+++ scripts/enrich-discovery.script.js
@@ -0,0 +1,32 @@
+var hosts = read("host-names.db.txt").split(',');
+var ONE_MILLION = 1000000;
+var TWO_DECIMAL_PLACES = 2;
+
+result = [];
+
+for (var i = 0; i < hosts.length; i++) {
+  var target = hosts[i];
+  print('=========' + target + ' (' + calculateProgress(i) + '% done)' + '=========')
+  result.push(createServerInfoObject(target));
+}
+
+write('hosts-info.db', JSON.stringify(result), 'w');
+
+function calculateProgress(currentIndex) {
+  return (((currentIndex + 1) / hosts.length) * 100).toFixed(TWO_DECIMAL_PLACES);
+}
+
+function createServerInfoObject(target) {
+  return {
+    hostName: target,
+    hasRootAccess: hasRootAccess(target),
+    hackChancePercentage: Math.floor(hackChance(target) * 100),
+    maxMoneyInMillions: parseFloat((getServerMaxMoney(target) / ONE_MILLION).toFixed(TWO_DECIMAL_PLACES)),
+    availableMoneyInMillions: parseFloat((getServerMoneyAvailable(target) / ONE_MILLION).toFixed(TWO_DECIMAL_PLACES)),
+    numberOfPortsRequired: getServerNumPortsRequired(target),
+    hackTimeInSeconds: getHackTime(target).toFixed(TWO_DECIMAL_PLACES),
+    growTimeInSeconds: getGrowTime(target).toFixed(TWO_DECIMAL_PLACES),
+    weakenTimeInSeconds: getWeakenTime(target).toFixed(TWO_DECIMAL_PLACES)
+  };
+
+}
